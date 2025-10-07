56d14eeb416913aeb88762baf6c39f53753aabf3
Andrew Schtkan <texview@gmx.com>
2025-10-08 00:01:47 +0300

    IUserContextService wasn't completed. Fix

--- CRUDaster.Core/Application/Interfaces/IUserContextService.cs
+++ CRUDaster.Core/Application/Interfaces/IUserContextService.cs
@@ -3,5 +3,9 @@
     public interface IUserContextService
     {
         string? GetUserId();
+        public string? GetUserName();
+        public string? GetUserEmail();
+        public bool IsInRole(string role);
+        public IEnumerable<string> GetUserRoles();
     }
 }
